738a924ee1fc81af6ae0812c82c6f9bed9e31235
hiraBiswas <hbhirabiswas@gmail.com>
2024-04-13 00:30:11 +0600

    user created means registered succesfully

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -8,9 +8,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useForm } from 'react-hook-form';
 import useAxiosPublic from '../../hooks/useAxiosPublic'
 
-const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
-const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
-
 const Register = () => {
     const {
         register,
@@ -24,101 +21,22 @@ const Register = () => {
     const { createUser } = useContext(AuthContext)
     const axiosPublic = useAxiosPublic();
 
-    //   const handleRegister = async (e) => {
-    //     e.preventDefault();
-
-    //     const form = new FormData(e.target);
-    //     const name = form.get('name');
-    //     const photo = form.get('photo');
-    //     const email = form.get('email');
-    //     const password = form.get('password');
-    //     const confirmPassword = form.get('confirmPassword');
-    //     const courseLevel = form.get('courseLevel');
-    //     const father_name = form.get('father_name');
-    //     const mother_name = form.get('mother_name');
-    //     const gender = form.get('gender');
-    //     const religion = form.get('religion');
-    //     const nationality = form.get('nationality');
-    //     const nationalOrBirth = form.get('nationalOrBirth');
-    //     const presentAddress = form.get('presentAddress');
-    //     const permanentAddress = form.get('permanentAddress');
-    //     const mobileNumber = form.get('mobileNumber');
-    //     const guardianMobileNumber = form.get('guardianMobileNumber');
-    //     const educationLevel = form.get('educationLevel');
-    //     const subjectDiplomaGroup = form.get('subjectDiplomaGroup');
-    //     const institute = form.get('institute');
-    //     const trainee_institute = form.get('trainee_institute');
-    //     const location = form.get('location');
-
-    //     console.log(name, email, password, photo, location);
-
-
-    //     const imageFile = photo[0];
-    //     const formData = new FormData();
-    //     formData.append("image", imageFile);
-
-    //     try {
-    //         const response = await fetch(image_hosting_api, {
-    //             method: "POST",
-    //             body: formData,
-    //         });
-
-    //         if (!response.ok) {
-    //             throw new Error(`HTTP error! Status: ${response.status}`);
-    //         }
-
-    //         const res = await response.json();
-    //         if (!res.success) {
-    //             toast.error("Error during image upload");
-    //             return;
-    //         }
-
-    //         if (password.length < 6) {
-    //             toast.error('Password should be at least 6 characters long');
-    //             return;
-    //         }
-    //         if (!/[A-Z]/.test(password)) {
-    //             toast.error('Password should contain at least one capital letter');
-    //             return;
-    //         }
-    //         if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password)) {
-    //             toast.error('Password should contain at least one special character');
-    //             return;
-    //         }
-
-    //         createUser(name, photo, email, password)
-    //             .then(result => {
-    //                 console.log(result.user)
-    //                 updateProfile(auth.currentUser, {
-    //                     displayName: name,
-    //                     photoURL: photo
-    //                 })
-    //                 .then(result => {
-    //                     toast.success('Registration Successful')
-    //                     navigate(location?.state ? location.state : '/')
-    //                 })
-    //                 .catch(error => {
-    //                     toast.error(error.message)
-    //                 })
-
-    //             })
-    //             .catch(error => {
-    //                 toast.error(error.message)
-    //                 console.error("Registration error:", error.message)
-    //             })
-
-    //     } catch (error) {
-    //         toast.error(error.message);
-    //     }
-    // };
-
-
+    
 
     const onSubmit = async (data) => {
         try {
-            const { } = data;
+            const { password, confirmPassword, name, email, roll, department, year, institution, prefCourse } = data;
             console.log(data)
+
+            // Set photo URL and user type
+            const image = 'https://i.ibb.co/JvWtdNv/anonymous-user-circle-icon-vector-illustration-flat-style-with-long-shadow-520826-1931.jpg';
+            const type = 'student';
+
             // Password validation
+            if (password !== confirmPassword) {
+                toast.error("Passwords do not match");
+                return;
+            }
             if (password.length < 6) {
                 toast.error("Password should be at least 6 characters long");
                 return;
@@ -132,62 +50,20 @@ const Register = () => {
                 return;
             }
 
-            console.log(data)
-            // Image upload
-            const imageFile = data.image[0];
-            const formData = new FormData();
-            formData.append("image", imageFile);
-
-            const response = await fetch(image_hosting_api, {
-                method: "POST",
-                body: formData,
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const res = await response.json();
-            if (!res.success) {
-                toast.error("Error during image upload");
-                return;
-            }
-
-            console.log(res.data.display_url)
+            const assignedCourse = ''; 
+    
             // Firebase authentication
-            createUser(name, res.data.display_url, type, email, password)
+            createUser(name, image, type, email, password)
                 .then(() => {
                     updateProfile(auth.currentUser, {
                         displayName: name,
-                        photoURL: res.data.display_url,
+                        photoURL:image
+
                     })
                         .then(() => {
+                       
                             // Database upload
-                            const userInfo = {
-                                name: data.name,
-                                email: data.email,
-                                type: 'user',
-                                contact: data.contact,
-                                // fatherName: data.fatherName,
-                                // motherName: data.motherName,
-                                // gender: data.gender,
-                                // religion: data.religion,
-                                // nationality: data.nationality,
-                                // nationalOrBirth: data.nationalOrBirth,
-                                // presentAddress: data.presentAddress,
-                                // permanentAddress: data.permanentAddress,
-                                // guardianMobileNumber: data.guardianMobileNumber,
-                                // educationLevel: data.educationLevel,
-                                // subjectDiplomaGroup: data.subjectDiplomaGroup,
-                                // //   institute:data.institute,
-                                // traineeInstitute: data.traineeInstitute,
-
-                                // image: res.data.display_url,
-
-
-                            };
-
-                            axiosPublic.post('/users', userInfo)
+                            axiosPublic.post('/users', { name, email, roll, department, year, institution, prefCourse, image, type, assignedCourse })
                                 .then(res => {
                                     if (res.data.insertedId) {
                                         // Success
@@ -207,11 +83,13 @@ const Register = () => {
                 .catch((error) => {
                     toast.error(`User creation error: ${error.message}`);
                 });
-
+    
         } catch (error) {
             toast.error(`Error during form submission: ${error.message}`);
         }
     };
+    
+    
     return (
         <div>
             <div className="  text-center mt-8">
@@ -245,7 +123,7 @@ const Register = () => {
                                     <label className="label">
                                         <span className="label-text text-lg font-medium">Email *</span>
                                     </label>
-                                    <input {...register('email', { required: true })} type="email" placeholder="email" name="contact" className="input input-bordered  " required />
+                                    <input {...register('email', { required: true })} type="email" placeholder="email" name="email" className="input input-bordered  " required />
                                 </div>
 
 
@@ -267,16 +145,16 @@ const Register = () => {
                                     <label className="label">
                                         <span className="label-text text-lg font-medium">Year & Semester *</span>
                                     </label>
-                                    <select {...register('year', { required: true })} className="select select-bordered ml-2" required>
+                                    <select {...register('year', { required: true })}  className="select select-bordered ml-2" required>
                                         <option value="">Select Year & Semester</option>
-                                        <option value="1">Year 1 Semester 1</option>
-                                        <option value="2">Year 1 Semester 2</option>
-                                        <option value="3">Year 2 Semester 1</option>
-                                        <option value="4">Year 2 Semester 2</option>
-                                        <option value="5">Year 3 Semester 1</option>
-                                        <option value="6">Year 3 Semester 2</option>
-                                        <option value="7">Year 4 Semester 1</option>
-                                        <option value="8">Year 4 Semester 2</option>
+                                        <option value="1st Year 1st Semester">1st Year 1st Semester</option>
+                                        <option value="1st Year 2nd Semester">1st Year 2nd Semester</option>
+                                        <option value="2nd Year 1st Semester">2nd Year 1st Semester </option>
+                                        <option value="2nd Year 2nd Semester">2nd Year 2nd Semester </option>
+                                        <option value="3rd Year 1st Semester">3rd Year 1st Semester</option>
+                                        <option value="3rd Year 2nd Semester">3rd Year 2nd Semester</option>
+                                        <option value="4th Year 1st Semester">4th Year 1st Semester</option>
+                                        <option value="4th Year 2nd Semester">4th Year 2nd Semester</option>
 
 
                                     </select>
@@ -299,7 +177,13 @@ const Register = () => {
                                         <label className="label ">
                                             <span className="label-text text-lg font-medium">Preferable Course *</span>
                                         </label>
-                                        <input {...register('prefCourse', { required: true })} type="text" placeholder="preferable course" className="input input-bordered" name="traineeInstitute" required />
+                                        <select {...register('prefCourse', { required: true })} className="select select-bordered ml-2" required>
+                                        <option value="">Select Preferable Course</option>
+                                        <option value="Data Visualization with Python">Data Visualization with Python</option>
+                                        <option value="Database">Database</option>
+                                        <option value="Basic Programming with Python">Basic Programming with Python</option>
+                                        <option value="Front End Development">Front End Development </option>
+                                    </select>
 
                                     </div>
                                 </div>
@@ -309,14 +193,14 @@ const Register = () => {
                                     <label className="label">
                                         <span className="label-text text-lg font-medium">Password</span>
                                     </label>
-                                    <input {...register('password', { required: true })} type="text" placeholder="password" name="password" className="input input-bordered" required />
+                                    <input {...register('password', { required: true })} type="password" placeholder="password" name="password" className="input input-bordered" required />
                                 </div>
 
                                 <div className="form-control flex  ">
                                     <label className="label">
                                         <span className="label-text text-lg font-medium">Confirm Password</span>
                                     </label>
-                                    <input {...register('confirmPassword', { required: true })} type="text" placeholder="confirm password" name="confirmPassword" className="input input-bordered" required />
+                                    <input {...register('confirmPassword', { required: true })} type="password" placeholder="confirm password" name="confirmPassword" className="input input-bordered" required />
                                 </div>
                          </div>
                     
